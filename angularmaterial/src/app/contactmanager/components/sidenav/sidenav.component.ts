91f80051750e7b4eac1542a3f2ba9dd11142b9c0
Jie Zhou (JAY) <jiz156@pitt.edu>
2021-08-11 18:19:11 -0400

    Set up data service

--- angularmaterial/src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ angularmaterial/src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,5 +1,8 @@
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 const SMALL_WIDTH_BREAKPOINT = 720;
 
@@ -12,13 +15,23 @@ export class SidenavComponent implements OnInit {
 
   public isScreenSmall: boolean | undefined;
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  users!: Observable<User[]>;
+
+  constructor(private breakpointObserver: BreakpointObserver,
+              private userService: UserService) { }
 
   ngOnInit(): void {
     this.breakpointObserver
     .observe([`(max-width:  ${SMALL_WIDTH_BREAKPOINT}px)`])
     .subscribe((state: BreakpointState)=> {
       this.isScreenSmall = state.matches;
+    });
+
+    this.users = this.userService.users;
+    this.userService.loadAll();
+
+    this.users.subscribe(data => {
+      console.log(data);
     })
   }
 }
