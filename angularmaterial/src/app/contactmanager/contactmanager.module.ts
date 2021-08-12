829a8e348cf83e6a499121a93055b1c734eed809
Jie Zhou (JAY) <jiz156@pitt.edu>
2021-08-12 10:53:59 -0400

    Add in display for user cards

--- angularmaterial/src/app/contactmanager/contactmanager.module.ts
+++ angularmaterial/src/app/contactmanager/contactmanager.module.ts
@@ -18,9 +18,9 @@ const routes: Routes = [
   {
     path: '', component: ContactmanagerAppComponent,
     children: [
+      {path: ':id', component: MainContentComponent},
       {path: '', component: MainContentComponent}
-    ]
-  },
+    ]},
   {path: '**', redirectTo:''}
 ]
 
--- angularmaterial/src/app/contactmanager/services/user.service.ts
+++ angularmaterial/src/app/contactmanager/services/user.service.ts
@@ -23,14 +23,18 @@ export class UserService {
     return this._users.asObservable();
   }
 
-   loadAll(){
-     const usersUrl = 'https://angular-material-api.azurewebsites.net/users';
-     return this.http.get<User[]>(usersUrl)
-     .subscribe( data => {
-       this.dataStore.users = data;
-       this._users!.next(Object.assign({}, this.dataStore).users);
-     }, error => {
-       console.log("Failed to fetch users")
-     });
-   }
+  userById(id: number){
+    return this.dataStore.users.find(x => x.id == id);
+  }
+
+  loadAll(){
+    const usersUrl = 'https://angular-material-api.azurewebsites.net/users';
+    return this.http.get<User[]>(usersUrl)
+    .subscribe( data => {
+      this.dataStore.users = data;
+      this._users!.next(Object.assign({}, this.dataStore).users);
+    }, error => {
+      console.log("Failed to fetch users")
+    });
+  }
 }
